392e6a4442b30f670440b05c91320d52eecc1bdd
Juan Cruz Albrieu <jc.alb@hotmail.com>
2019-07-31 11:09:13 -0300

    Initial commit

---
+++ Proyecto/Assets/Script/DestruirPorContacto.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruirPorContacto : MonoBehaviour
+{
+
+    public GameObject explosion,playerExplosion;
+    private int puntaje= 10;
+    private GameController gamecontroller;
+
+    private void Start()
+    {
+        gamecontroller = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag != "limite")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+
+            Destroy(Instantiate(explosion, transform.position, transform.rotation),3);
+            if (other.CompareTag("Player"))
+            {
+                Destroy(Instantiate(playerExplosion, other.transform.position, other.transform.rotation),2);
+            }
+            else
+            {
+                Debug.Log("DestruirPorCOntacto: entra "+puntaje);
+                gamecontroller.addScore(puntaje); 
+            }
+            
+        }
+    }
+}
