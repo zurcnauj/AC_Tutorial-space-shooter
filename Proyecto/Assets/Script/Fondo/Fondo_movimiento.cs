31725349853127310d68361eb207e5aa512c4741
Juan Cruz Albrieu <jc.alb@hotmail.com>
2019-07-31 19:13:33 -0300

    asdf

---
+++ Proyecto/Assets/Script/Fondo/Fondo_movimiento.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fondo_movimiento : MonoBehaviour
+{
+    [Header("Movimiento")]
+    public float velocidad;
+
+    [Header("Posicion")]
+    private Vector3 inicial, actual;
+
+    private void Awake()
+    {
+        inicial = transform.position;
+        actual = inicial;
+    }
+
+    private void FixedUpdate()
+    {
+        if(actual.y < -30)
+        {
+            actual = inicial;
+        }
+        else
+        {
+            actual.y -= velocidad;
+        }
+
+        transform.position = actual;
+    }
+}
